469c7dca830379737a05a303d85eda1196d41d07
Shadman Shaikh <shadmanforreal45@gmail.com>
2025-02-07 23:02:51 +0400

    added whole project

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Auth;
+
+Volt::route('/', 'dashboard.dashboard')->name('home')->middleware('auth');
+Volt::route('/login' , 'login.login')->name('login'); 
+Route::get('/logout' , function(){
+    Auth::logout();
+    session()->invalidate();
+    session()->regenerateToken();
+    return redirect()->route('login')->with('status', 'You have been logged out.')->withHeaders([
+        'Cache-Control' => 'no-cache, no-store, max-age=0, must-revalidate',
+        'Pragma' => 'no-cache',
+        'Expires' => 'Fri, 01 Jan 1990 00:00:00 GMT'
+    ]);
+});
